41e5560016a1408b253be9b4061aa6fa8e0ca39a
Tushyaansh Pal <tushyaanshpal3@gmail.com>
2026-01-14 10:56:38 +0530

    L-169 Moore's Voting Algo

--- Problems/Arrays/L-169_Majority_Element.cpp
+++ Problems/Arrays/L-169_Majority_Element.cpp
@@ -1,5 +1,20 @@
-
-
+//OPTIMAL -> MOORE's VOTING ALGO
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=0,element;
+        for(int i=0;i<nums.size();i++){
+        if(count == 0){
+            count =1;
+            element = nums[i];
+        }
+        else if (nums[i]==element)
+        count++;
+        else count--;
+        }
+        return element;
+    }
+};
 
 
 
