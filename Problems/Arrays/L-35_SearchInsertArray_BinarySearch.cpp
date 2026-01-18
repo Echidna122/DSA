640a4d4f627b85e40a71a6fce17680f4bd62f3c6
Tushyaansh Pal <tushyaanshpal3@gmail.com>
2026-01-18 23:36:05 +0530

    L-35 Search Insert Array, Binary Sort

---
+++ Problems/Arrays/L-35_SearchInsertArray_BinarySearch.cpp
@@ -0,0 +1,26 @@
+//OPTIMAL (MINE)
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low =0,high = n-1;
+        int ind=0;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid]<target){
+                low = mid+1;
+                ind = mid+1;
+            }
+            else if(nums[mid]>target){
+                high = mid-1;
+                ind = mid;
+            }
+            else{
+                ind = mid;
+                return ind;
+            }
+
+        }
+        return ind;
+    }
+};
